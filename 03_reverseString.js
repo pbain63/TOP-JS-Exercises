5a33bc3a9f9d633d404050334ee0d511fce641bb
pbain63 <prodipbain63@gmail.com>
2024-02-23 17:58:45 +0600

    Add solution of reverse problem

---
+++ 03_reverseString.js
@@ -0,0 +1,32 @@
+//freecodecamp.org/news/how-to-reverse-a-string-in-
+//javascript-in-3-different-ways-
+//--Using decrementing for loop
+const reverseString = function (word) {
+    if (word === "") return "";
+    let newString = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+      newString += word[i];
+    }
+    return newString;
+  };
+  //--
+  //--
+  //Using built-in functions
+  // const reverseString = function (word) {
+  //   if (word === "") {
+  //     return "";
+  //   } else {
+  //     return word.split("").reverse().join("");
+  //   }
+  // };
+  //--
+  //--
+  //TOP solution:
+  // const reverseString = function (string) {
+  //   return string.split("").reverse().join("");
+  // };
+  
+  //--
+  // Do not edit below this line
+  module.exports = reverseString;
+  
