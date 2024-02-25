5171b5adcf3ab6169e7f5d2ec163a27a5107096e
pbain63 <prodipbain63@gmail.com>
2024-02-25 16:34:58 +0600

    Modify 07_tempConversion.js

---
+++ 07_tempConversion.js
@@ -0,0 +1,16 @@
+const convertToCelsius = function (tempCel) {
+    let fahrenheit = (tempCel - 32) * (5 / 9);
+    return Math.round(fahrenheit * 10) / 10;
+  };
+  
+  const convertToFahrenheit = function (tempFar) {
+    let celsius = tempFar * (9 / 5) + 32;
+    return Math.round(celsius * 10) / 10;
+  };
+  
+  // Do not edit below this line
+  module.exports = {
+    convertToCelsius,
+    convertToFahrenheit,
+  };
+  
