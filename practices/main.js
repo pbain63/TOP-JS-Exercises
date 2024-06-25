4a05f8f6aeaf9143936df48e8053521fb1cd4bf6
pbain63 <prodipbain63@gmail.com>
2024-06-25 20:33:21 +0600

    Add function(), event handler code in main.js

--- practices/main.js
+++ practices/main.js
@@ -8,3 +8,23 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/puppy.jpeg");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Welcome to the website, ${myName}.`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Hello, ${storedName}!`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
