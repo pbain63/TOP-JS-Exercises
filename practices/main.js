a446c17f969446db0b0887d432bd09958ac32676
pbain63 <prodipbain63@gmail.com>
2024-06-27 12:19:30 +0600

    Add code in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,14 +1,7 @@
-function sayHelloInFiveSeconds(name) {
-  var prompt = "Hello, " + name + "!";
-  // Inner functions are put in the local scope by default, as if they were
-  // declared with `var`.
-  function inner() {
-    alert(prompt);
-  }
-  setTimeout(inner, 5000);
-  // setTimeout is asynchronous, so the sayHelloInFiveSeconds function will
-  // exit immediately, and setTimeout will call inner afterwards. However,
-  // because inner is "closed over" sayHelloInFiveSeconds, inner still has
-  // access to the `prompt` variable when it is finally called.
-}
-sayHelloInFiveSeconds("Adam");
+myObj = {
+  myString: "Hello World!",
+  myFunc: function () {
+    return this.myString;
+  },
+};
+console.log(myObj.myFunc());
