1faeed115029396ccb3e1be65ccdf1c82d81d143
pbain63 <prodipbain63@gmail.com>
2024-06-28 23:54:13 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,8 +1,8 @@
-// function addOne(num) {
-//   return num + 1;
-// }
 const arr = [1, 2, 3, 4, 5];
-const mappedArr = arr.map((num) => num + 1);
-console.log(mappedArr);
-console.log(arr);
+const productOfAllNums = arr.reduce((total, currentItem) => {
+  return total * currentItem;
+}, 1);
+console.log(productOfAllNums); // Outputs 120;
+console.log(arr); // Outputs [1, 2, 3, 4, 5]
+
 // console.log(sumOfTripledEvens(1, 2, 3, 4, 5, 6, 7, 8));
