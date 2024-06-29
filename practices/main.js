3627e1161e05f60da22552570b9826ef3f7d56c3
pbain63 <prodipbain63@gmail.com>
2024-06-29 11:07:31 +0600

    Update writting section in index.html

--- practices/main.js
+++ practices/main.js
@@ -1,9 +1,14 @@
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
+  this.sayName = function () {
+    console.log(this.name);
+  };
 }
-const player = new Player("prodip", "X");
-console.log(player.name);
-console.log(player.marker);
+const player1 = new Player("prodip", "X");
+const player2 = new Player("Rock", "O");
+player1.sayName();
+player2.sayName();
+console.log(player1.marker);
+console.log(player1.name);
 // console.log(sumOfTripledEvens(1, 2, 3, 4, 5, 6, 7, 8));
-
