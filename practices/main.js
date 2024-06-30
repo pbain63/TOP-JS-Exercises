c2de881e2db4de1b7b98c3a42887052505feddb0
pbain63 <prodipbain63@gmail.com>
2024-06-30 12:12:57 +0600

    Add object JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,5 +1,13 @@
-const fruits = ["Apple", "Banana", "Watermelon", "Orange"];
+const buz = {
+  fog: "stack",
+};
 
-console.log(fruits.hasOwnProperty(4));
+for (const name in buz) {
+  if (buz.hasOwnProperty(name)) {
+    console.log(`this is fog (${name}) for sure. Value: ${buz[name]}`);
+  } else {
+    console.log(name);
+  }
+}
 
 // console.log(player1.name);
