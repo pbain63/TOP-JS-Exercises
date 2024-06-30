af8655df377f651374fd80bd514a6aabd1668c05
pbain63 <prodipbain63@gmail.com>
2024-06-30 16:25:50 +0600

    Add object JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -15,20 +15,25 @@ Player.prototype.getMarker = function () {
   console.log(`My marker is '${this.marker}'`);
 };
 
-Object.getPrototypeOf(Player.prototype);
+// Object.getPrototypeOf(Player.prototype);
 Object.setPrototypeOf(Player.prototype, Person.prototype);
+// Player.prototype = Person.prototype;
 
-Object.getPrototypeOf(Player.prototype);
+// Object.getPrototypeOf(Player.prototype);
 
 const player1 = new Player("prodip", "X");
 const player2 = new Player("also prodip", "O");
 
+const carl = new Player("carl", "^");
+
 player1.sayName();
 player2.sayName();
+carl.sayName();
 
 player1.getMarker();
 player2.getMarker();
+carl.getMarker();
 
-// console.log(Object.getPrototypeOf(Player.prototype));
+
 
 // console.log(player1.name);
