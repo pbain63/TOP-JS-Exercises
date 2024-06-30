eb155c9a23b667cd3624559ce32dc3309d6b6b3a
pbain63 <prodipbain63@gmail.com>
2024-06-30 16:03:52 +0600

    Add object JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,6 +1,34 @@
-const foo = { bar: "Here be dragons" };
-console.log(Object.hasOwn(foo, "bar"));
-console.log(Object.prototype.hasOwnProperty.call(foo, "bar"));
-console.log(({}).hasOwnProperty.call(foo, "bar"));
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayName = function () {
+  console.log(`Hello, I'm ${this.name}!`);
+};
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+}
+
+Player.prototype.getMarker = function () {
+  console.log(`My marker is '${this.marker}'`);
+};
+
+Object.getPrototypeOf(Player.prototype);
+Object.setPrototypeOf(Player.prototype, Person.prototype);
+
+Object.getPrototypeOf(Player.prototype);
+
+const player1 = new Player("prodip", "X");
+const player2 = new Player("also prodip", "O");
+
+player1.sayName();
+player2.sayName();
+
+player1.getMarker();
+player2.getMarker();
+
+// console.log(Object.getPrototypeOf(Player.prototype));
 
 // console.log(player1.name);
