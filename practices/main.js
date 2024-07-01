71f8d342d9ddb08deb6ceacae5bc6b45b6866df4
pbain63 <prodipbain63@gmail.com>
2024-07-01 14:48:32 +0600

    Add object JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,37 +1,18 @@
-function Hero(name, level) {
-  this.name = name;
-  this.level = level;
-}
-
-Hero.prototype.greet = function () {
-  return `${this.name} says hello!`;
+let animal = {
+  eats: true,
+  walk() {
+    console.log("Animal walk");
+  },
 };
-
-function Warrior(name, level, weapon) {
-  Hero.call(this, name, level);
-  this.weapon = weapon;
-}
-Warrior.prototype.attack = function () {
-  return `${this.name} attacks with the ${this.weapon}.`;
+let rabbit = {
+  jumps: true,
+  __proto__: animal,
 };
-
-function Healer(name, level, spell) {
-  Hero.call(this, name, level);
-  this.spell = spell;
-}
-Healer.prototype.heal = function () {
-  return `${this.name} casts ${this.spell}.`;
+let longEar = {
+  earLength: 10,
+  __proto__: rabbit,
 };
-
-Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
-Object.setPrototypeOf(Healer.prototype, Hero.prototype);
-
-const hero1 = new Warrior("Prodip", 1, "axe");
-const hero2 = new Healer("Bain", 1, "cure");
-
-console.log(hero1.attack());
-console.log(hero1.greet());
-
-// console.log(Object.getPrototypeOf(hero1));
-
+longEar.walk();
+console.log(longEar.eats);
+console.log(rabbit.jumps);
 // console.log(player1.name);
