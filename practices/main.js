3a6c764fa03917527ee857d6ce70e0fab3f95a22
pbain63 <prodipbain63@gmail.com>
2024-07-06 22:36:02 +0600

    Remove code in  main.js

--- practices/main.js
+++ practices/main.js
@@ -1,11 +1,3 @@
-const myDog = Object.create({
-  sayName() {
-    console.log(this === myDog);
-    return this.name;
-  },
-});
 
-myDog.name = "Milo";
-console.log(myDog.sayName());
 
 // console.log(player1.name);
