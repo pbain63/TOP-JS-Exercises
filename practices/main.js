535cb2752906f7fd7935eecb70505002b946cfa2
pbain63 <prodipbain63@gmail.com>
2024-07-06 22:38:12 +0600

    Add practice code for object in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,3 +1,16 @@
+class Planet {
+  constructor(name) {
+    this.name = name;
+  }
 
+  getName() {
+    console.log(this === earth); // => true
+    return this.name;
+  }
+}
+
+const earth = new Planet("Earth");
+// method invocation. the context is earth
+earth.getName(); // => 'Earth'
 
 // console.log(player1.name);
