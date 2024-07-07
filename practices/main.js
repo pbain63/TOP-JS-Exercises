4c2dd3829caac60fa156a47e5a65e2e879d30572
pbain63 <prodipbain63@gmail.com>
2024-07-07 17:05:29 +0600

    Add practice code for object in main.js

--- practices/main.js
+++ practices/main.js
@@ -2,14 +2,14 @@ function Pet(type, legs) {
   this.type = type;
   this.legs = legs;
 
-  this.ligInfo = function () {
+  this.ligInfo = () => {
     console.log(this === myCat);
     console.log(`The ${this.type} has ${this.legs} legs`);
   };
 }
 const myCat = new Pet("Cat", 4);
-const boundLogInfo = myCat.ligInfo.bind(myCat);
+// const boundLogInfo = myCat.ligInfo.bind(myCat);
 
-setTimeout(boundLogInfo, 1000);
+setTimeout(myCat.ligInfo, 1000);
 
 // console.log(player1.name);
