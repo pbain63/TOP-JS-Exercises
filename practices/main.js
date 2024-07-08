527950c7d775163630f3d902d08d2ec23da764c1
pbain63 <prodipbain63@gmail.com>
2024-07-08 07:58:10 +0600

    Add practice code for object in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,9 +1,7 @@
-class Foo {
-  constructor() {
-    this.property = "Default Value";
-  }
-  
-}
-const fooInstance = new Foo();
-console.log(fooInstance.property);
+const reg1 = new RegExp("\\w+");
+const reg2 = RegExp("\\w+");
+
+console.log(reg1 instanceof RegExp);
+console.log(reg2 instanceof RegExp);
+console.log(reg1.source === reg2.source);
 // console.log(player1.name);
