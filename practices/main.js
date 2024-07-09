148f4ca4677e81d784cc0854837a9839332f95cb
pbain63 <prodipbain63@gmail.com>
2024-07-09 16:14:57 +0600

    Add practice code for object in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,38 +1,15 @@
-function Student(name, grade) {
-  this.name = name;
-  this.grade = grade;
-}
-Student.prototype.sayName = function () {
-  console.log(this.name);
+const user = {
+  firstName: "Prodip",
+  lastname: "Bain",
+  hobbies: ["programming", "piano"],
+  listHobbies: function () {
+    
+    this.hobbies.forEach(function (hobby) {
+      console.log(hobby);
+    });
+  },
 };
-
-
-function EighthGrader(name) {
-  this.name = name;
-  this.grade = 8;
-}
-EighthGrader.prototype = Object.create(Student.prototype);
-
-function NinthGrader(name) {
-  this.name = name;
-  this.grade = 9;
-}
-
-// Student.prototype.goToProm = function () {
-//   console.log("Eh.. go to prom?");
-// };
-NinthGrader.prototype = Object.create(Student.prototype);
-
-NinthGrader.prototype.sayName = function () {
-  console.log("HAHAHHAHAHAHAHA");
-};
-
-
-const studentOne = new NinthGrader("steve");
-const studentTwo = new EighthGrader("jenn");
-
-studentOne.sayName();
-studentTwo.sayName();
+user.listHobbies();
 
 // console.dir(Object);
 // console.log(player1.name);
