f5e34f68f4d1235b9dcae6d78cefdbf7c7095534
pbain63 <prodipbain63@gmail.com>
2024-07-09 13:24:31 +0600

    Add practice code for object in main.js

--- practices/main.js
+++ practices/main.js
@@ -6,15 +6,34 @@ const playerTwoMarker = "O";
 const playerOne = {
   name: "tim",
   marker: "X",
+  score: 15,
 };
 const playerTwo = {
   name: "jenn",
   marker: "O",
+  score: 5,
 };
-function printName(player) {
-  console.log(player.name);
+function keepScore() {
+  if (playerOne.score >= 10) {
+    gameOver(playerOne);
+  } else if (playerTwo.score >= 10) {
+    gameOver(playerTwo);
+  }
 }
-printName(playerTwo);
+
+keepScore();
+
+function gameOver(winningPlayer) {
+  console.log("Congratulations!");
+  console.log(winningPlayer.name + " is the winning player.");
+}
+
+
+
+// function printName(player) {
+//   console.log(player.name);
+// }
+// printName(playerTwo);
 
 // console.dir(Object);
 // console.log(player1.name);
