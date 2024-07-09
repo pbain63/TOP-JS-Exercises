eeb5a4716cde37dd845be8001859d5894ecdaaf8
pbain63 <prodipbain63@gmail.com>
2024-07-09 16:26:47 +0600

    Add practice code for object in main.js

--- practices/main.js
+++ practices/main.js
@@ -3,13 +3,17 @@ const user = {
   lastname: "Bain",
   hobbies: ["programming", "piano"],
   listHobbies: function () {
-    
+
     this.hobbies.forEach(function (hobby) {
+      // console.log(user.firstName);
+      console.log(this.firstName);
       console.log(hobby);
-    });
+    }, this);
   },
 };
 user.listHobbies();
+
+
 
 // console.dir(Object);
 // console.log(player1.name);
