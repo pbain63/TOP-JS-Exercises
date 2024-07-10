89fb76c9831fedd886f7cd52553d8eed2e20f9b3
pbain63 <prodipbain63@gmail.com>
2024-07-10 22:20:16 +0600

    Remove js code from main.js

--- practices/main.js
+++ practices/main.js
@@ -1,10 +1,4 @@
-function User(name) {
-  this.name = name;
-  console.log(this);
-}
 
-const prodip = new User("Prodip");
-const bain = new User("Bain");
 
 // console.dir(Object);
 // console.log(player1.name);
