d2be431d40731800f5e5afb5edfb7d8cd2d174fa
pbain63 <prodipbain63@gmail.com>
2024-07-12 09:17:19 +0600

    Add dialog JS code to main.js

---
+++ practices/main.js
@@ -0,0 +1,14 @@
+const showBtn = document.getElementById("show-dialog");
+const dialog = document.getElementById("dialog");
+const jsCloseBtn = dialog.querySelector("#js-close");
+
+showBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+jsCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.close();
+});
+
+// console.log();
