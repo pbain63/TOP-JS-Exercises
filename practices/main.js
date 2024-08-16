0d5506bb8950bc57bc3b10151ab995039945a226
pbain63 <prodipbain63@gmail.com>
2024-08-16 20:45:10 +0600

    Add JS code for practice in main .js

--- practices/main.js
+++ practices/main.js
@@ -1,12 +1,24 @@
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayName = function () {
+  console.log(`Hello, I'm ${this.name}!`);
+};
+
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
-  this.sayName = function () {
-    console.log(this.name);
-  };
 }
 
+Player.prototype.getMarker = function () {
+  console.log(`My marker is '${this.marker}'`);
+};
+
+Object.setPrototypeOf(Player.prototype, Person.prototype);
+
 const player1 = new Player("Prodip", "X");
-const player2 = new Player("Also Prodip", "O");
+const player2 = new Player("also Prodip", "O");
 
 player1.sayName();
+player2.sayName();
