06d1a8b5938b2d679599a55c0271e82a01b950e1
pbain63 <prodipbain63@gmail.com>
2024-08-22 22:14:34 +0600

    Add JS code to main.js

---
+++ practices/main.js
@@ -0,0 +1,13 @@
+var data = [
+  {
+    errorCode: 100,
+    message: {},
+    name: "InternetGatewayDevice.LANDevice.1.Hosts.HostNumberOfEntries",
+    value: "2",
+  },
+];
+
+// data.map((x) => x.value);
+data.map((x) => console.log(x.value));
+
+data.forEach((x) => console.log(x.value));
