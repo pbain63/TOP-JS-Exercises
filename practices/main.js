4992413f2bee494d10091318bbc59fc21254e878
pbain63 <prodipbain63@gmail.com>
2024-10-13 22:56:35 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -2,5 +2,15 @@ let globalAge = 23;
 
 function printAge(age) {
     var varAge = 34;
+
+    if (age > 0) {
+        const constAge  = age * 2;
+        console.log(constAge);
+        
+    }
+    console.log(constAge);
     
+
 }
+printAge(constAge);
+
