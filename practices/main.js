1f7b78479b24e8a0669fb91b462c3a7462239ab2
pbain63 <prodipbain63@gmail.com>
2024-10-19 22:23:06 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,4 +1,8 @@
 function makeAdding(firstNUmber) {
   const first = firstNUmber;
-  
+
+  return function resulting(secondNumber) {
+    const second = secondNumber;
+    return first + second;
+  }
 }
