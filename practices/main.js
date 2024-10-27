6282afff714a7a0676b8bb9b66ceebadd8722565
pbain63 <prodipbain63@gmail.com>
2024-10-27 20:57:08 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,11 +1,6 @@
-function makeAdder(x) {
-  return function (y) {
-    return x + y;
-  };
+if (Math.random() > 0.5) {
+  var x = 1;
+} else {
+  var x = 2;
 }
-
-const add5 = makeAdder(5);
-const add10 = makeAdder(10);
-
-console.log(add5(2));
-console.log(add10(2));
+console.log(x);
