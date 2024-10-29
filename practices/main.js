0862ad4ff4ffdefae895ac8564e938322b5aecc6
pbain63 <prodipbain63@gmail.com>
2024-10-29 19:19:01 +0600

    Add factory function for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,4 +1,9 @@
 const User = function (name) {
   this.name = name;
   this.discordName = "@" + name;
+};
+
+function createUser(name) {
+  const discordName = "@" + name;
+  return { name, discordName };
 }
