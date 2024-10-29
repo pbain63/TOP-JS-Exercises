96cdb0e896436d3da11cd23c2a60fb35dfd69b16
pbain63 <prodipbain63@gmail.com>
2024-10-29 19:15:33 +0600

    Add constructor function for practice in index.html

--- practices/main.js
+++ practices/main.js
@@ -1,13 +1,4 @@
-function makeSizer(size) {
-  return function () {
-    document.body.style.fontSize = `${size}px`;
-  };
+const User = function (name) {
+  this.name = name;
+  this.discordName = "@" + name;
 }
-
-const size12 = makeSizer(12);
-const size14 = makeSizer(14);
-const size16 = makeSizer(36);
-
-document.getElementById("size-12").onclick = size12;
-document.getElementById("size-14").onclick = size14;
-document.getElementById("size-16").onclick = size16;
