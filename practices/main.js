b82b5eb49e1dacf7297e9dad599cd191ffc36951
pbain63 <prodipbain63@gmail.com>
2024-11-24 20:59:38 +0600

    Add JS code for practice in main.js

---
+++ practices/main.js
@@ -0,0 +1,7 @@
+const foo = ["one", "two"];
+
+const [red, yellow, green, blue] = foo;
+console.log(red); // "one"
+console.log(yellow); // "two"
+console.log(green); // undefined
+console.log(blue); // undefined
