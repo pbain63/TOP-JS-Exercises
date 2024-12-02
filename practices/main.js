bf0a505a12c93d0696d7ca512308af025a3933fb
pbain63 <prodipbain63@gmail.com>
2024-12-02 21:45:19 +0600

    Add JS code for practice in main.js

---
+++ practices/main.js
@@ -0,0 +1,10 @@
+const obj = {
+    *[Symbol.iterator]() {
+      for (const v of [0, 1, 2, 3]) {
+        console.log(v);
+        yield v;
+      }
+    },
+  };
+  const [a, b] = obj; // Only logs 0 and 1
+  
