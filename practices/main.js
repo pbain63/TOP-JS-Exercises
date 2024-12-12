c14d3b3a6842916d74ecb4ea907cf0703b3113bc
pbain63 <prodipbain63@gmail.com>
2024-12-12 10:16:55 +0600

    Add JS code for practice in main.js

---
+++ practices/main.js
@@ -0,0 +1,13 @@
+function drawChart({
+  size = "big",
+  coords = { x: 0, y: 0 },
+  radius = 25,
+} = {}) {
+  console.log(size, coords, radius);
+  // do some chart drawing
+}
+
+drawChart({
+  coords: { x: 18, y: 30 },
+  radius: 30,
+});
