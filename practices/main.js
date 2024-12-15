e6b1be54db99fd23202e05cc4718df719aac8634
pbain63 <prodipbain63@gmail.com>
2024-12-15 21:37:14 +0600

    Add JS code for practice in main.js

---
+++ practices/main.js
@@ -0,0 +1,25 @@
+const metadata = {
+  title: "Scratchpad",
+  translations: [
+    {
+      locale: "de",
+      localizationTags: [],
+      lastEdit: "2014-04-14T08:43:37",
+      url: "/de/docs/Tools/Scratchpad",
+      title: "JavaScript-Umgebung",
+    },
+  ],
+  url: "/en-US/docs/Tools/Scratchpad",
+};
+
+const {
+  title: englishTitle, // rename
+  translations: [
+    {
+      title: localeTitle, // rename
+    },
+  ],
+} = metadata;
+
+console.log(englishTitle); // "Scratchpad"
+console.log(localeTitle); // "JavaScript-Umgebung"
