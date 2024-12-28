695ed6dad0c68971d496fafcff86359bb06a16e3
pbain63 <prodipbain63@gmail.com>
2024-12-28 15:05:48 +0600

    Add JS code for practice in main.js

---
+++ practices/main.js
@@ -0,0 +1,14 @@
+const User = function (name) {
+    this.name = name;
+    this.discordName = "@" + name;
+  }
+  // hey, this is a constructor - 
+  // then this can be refactored into a factory!
+  
+  function createUser (name) {
+    const discordName = "@" + name;
+    return { name, discordName };
+  }
+  // and that's very similar, except since it's just a function,
+  // we don't need a new keyword
+  
