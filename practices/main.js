235bdd3635fc53dec48b466e01609af143bb2d0c
pbain63 <prodipbain63@gmail.com>
2025-01-06 22:28:57 +0600

    Remove JS code for practice in main.js

--- practices/main.js
+++
@@ -1,9 +0,0 @@
-// If you wanted to log these values, earlier,
-// you would have done the following
-console.log(name, age, color);
-// which would have resulted in a mess - Bob 28 red
-
-// Try wrapping it in some { curly braces } now,
-// which makes it an object!
-console.log({ name, age, color });
-// now it logs as - { name: "Bob", age: 28, color: "red" }
