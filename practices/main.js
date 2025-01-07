df8d82b39dfb653764b6f32fe232b39c720e065e
pbain63 <prodipbain63@gmail.com>
2025-01-07 21:43:56 +0600

    Add JS code for practice in main.js

---
+++ practices/main.js
@@ -0,0 +1,11 @@
+const obj = { a: 1, b: 2 };
+const { a, b } = obj;
+// This creates two variables, a and b,
+// which are equivalent to
+// const a = obj.a;
+// const b = obj.b;
+
+const array = [1, 2, 3, 4, 5];
+const [ zerothEle, firstEle ] = array;
+// This creates zerothEle and firstEle, both of which point
+// to the elements in the 0th and 1st indices of the array
