7efb2339ff00be9832a3b64fd8d5cd098dbd42e0
pbain63 <prodipbain63@gmail.com>
2025-01-13 23:05:01 +0600

    Add JS code for practice in main.js

---
+++ practices/main.js
@@ -0,0 +1,7 @@
+function createPlayer (name, level) {
+    const { getReputation, giveReputation } = createUser(name);
+  
+    const increaseLevel = () => level++;
+    return { name, getReputation, giveReputation, increaseLevel };
+  }
+  
