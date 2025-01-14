62bc4f12a5960cd9d39c1a4a6c2dfa0d6b00452f
pbain63 <prodipbain63@gmail.com>
2025-01-14 23:07:03 +0600

    Remove JS code for practice in main.js

--- practices/main.js
+++
@@ -1,7 +0,0 @@
-function createPlayer (name, level) {
-    const { getReputation, giveReputation } = createUser(name);
-  
-    const increaseLevel = () => level++;
-    return { name, getReputation, giveReputation, increaseLevel };
-  }
-  
