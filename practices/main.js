4ebd6c32837138128f8090b56b8783b4d1da3c46
pbain63 <prodipbain63@gmail.com>
2025-01-22 13:49:00 +0600

    Add js code for lexical scoping practice

--- practices/main.js
+++ practices/main.js
@@ -1,12 +1,10 @@
-function makeAdding(firstNumber) {
-  const first = firstNumber;
-  return function resulting(secondNumber) {
-    const second = secondNumber;
-    return first + second;
-  };
+function init() {
+  let name = "PRODIP";
+
+  function displayName() {
+    console.log(name);
+  }
+  displayName();
 }
 
-const add5 = makeAdding(5);
-// console.log(add5(2));
-const resulting7 = add5(2);
-console.log(resulting7);
+init();
