4f2814219ebd04b93c60360b435888c8b23302ec
pbain63 <prodipbain63@gmail.com>
2025-01-22 10:42:18 +0600

    Add js code for closure practice

--- practices/main.js
+++ practices/main.js
@@ -1,13 +1,12 @@
-let globalAge = 23;
-function printAge(age) {
-  var varAge = 34;
-
-  if (age > 0) {
-    const constAge = age * 2;
-    console.log(constAge);
-  }
-  console.log(constAge);
+function makeAdding(firstNumber) {
+  const first = firstNumber;
+  return function resulting(secondNumber) {
+    const second = secondNumber;
+    return first + second;
+  };
 }
-printAge(globalAge);
 
-console.log(varAge);
+const add5 = makeAdding(5);
+// console.log(add5(2));
+const resulting7 = add5(2);
+console.log(resulting7);
