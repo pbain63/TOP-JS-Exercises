91021476589177563dce08122da01399dcc391c1
pbain63 <prodipbain63@gmail.com>
2025-01-23 10:39:23 +0600

    Add js code for factory functions revision

--- practices/main.js
+++ practices/main.js
@@ -1,13 +1,11 @@
-function makeSizer(size) {
-  return function () {
-    document.body.style.fontSize = `${size}px`;
-  };
+const User = function (name) {
+  this.name = name;
+  this.discordName = "@"
+}
+function createUser(name) {
+  const discordName = "@" + name;
+  return {name, discordName};
 }
 
-const size12 = makeSizer(12);
-const size14 = makeSizer(14);
-const size16 = makeSizer(16);
 
-document.getElementById("size-12").onclick = size12;
-document.getElementById("size-14").onclick = size14;
-document.getElementById("size-16").onclick = size16;
+
