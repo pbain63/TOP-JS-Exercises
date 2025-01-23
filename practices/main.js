4be360ef3106de740f311cf17a0423bb25c3da33
pbain63 <prodipbain63@gmail.com>
2025-01-23 14:50:32 +0600

    Add js code for destructuring revision

--- practices/main.js
+++ practices/main.js
@@ -1,11 +1,10 @@
-const User = function (name) {
-  this.name = name;
-  this.discordName = "@"
-}
-function createUser(name) {
-  const discordName = "@" + name;
-  return {name, discordName};
-}
+// const array = [1, 2, 3, 4, 5];
+// const [zerothEle, firstEle] = array;
 
+// console.log(array[firstEle]);
+// console.log(firstEle);
 
-
+const numbers = [];
+const obj = { a: 1, b: 2 };
+({ a: numbers[0], b: numbers[1] } = obj);
+console.log(numbers[1]);
