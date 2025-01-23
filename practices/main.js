252288ecb22bf0353f222246ddcf046cd481c256
pbain63 <prodipbain63@gmail.com>
2025-01-23 18:04:30 +0600

    Add js code for destructuring revision

--- practices/main.js
+++ practices/main.js
@@ -1,10 +1,6 @@
-// const array = [1, 2, 3, 4, 5];
-// const [zerothEle, firstEle] = array;
+function f() {
+  return [1, 2];
+}
+const [a, b] = f();
 
-// console.log(array[firstEle]);
-// console.log(firstEle);
-
-let arr = [1, 20, 30];
-[arr[2], arr[1]] = [arr[1], arr[2]];
-
-console.log(arr);
+console.log(b);
