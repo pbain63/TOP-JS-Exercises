8bfa60de014fd1b37140c6e5a2f5362d48ccda80
pbain63 <prodipbain63@gmail.com>
2025-01-24 09:31:11 +0600

    Add JS code for destructure revision

--- practices/main.js
+++ practices/main.js
@@ -1,6 +1,12 @@
-function f() {
-  return [1, 2, 3];
-}
-const [a, , b] = f();
-const [c] = f();
-console.log(c);
+const obj = {
+  *[Symbol.iterator]() {
+    for (const v of [0, 1, 2, 3]) {
+      console.log(v);
+      yield v;
+    }
+  },
+};
+const [a, b, ...rest] = obj;
+console.log(rest);
+
+// console.log(obj);
