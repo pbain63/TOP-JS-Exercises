c4ce3ef0accc32a60b7244a7dc1886ee031b98ba
pbain63 <prodipbain63@gmail.com>
2025-01-24 15:39:25 +0600

    Add JS code for destructure revision

--- practices/main.js
+++ practices/main.js
@@ -1,4 +1,22 @@
-const {a: aa = 10, b:bb = 5} = {a:3};
-console.log(aa);
+const user = {
+  id: 42,
+  displayName: "jdoe",
+  fullName: {
+    firstName: "Jane",
+    lastName: "Doe",
+  },
+};
+function userId({ id }) {
+  return id;
+}
+console.log(userId(user));
 
-console.log(bb);
+function userDisplayName({ displayName: dname }) {
+  return dname;
+}
+console.log(userDisplayName(user));
+
+function whois({ displayName, fullName: { firstName: name } }) {
+  return `${displayName} is ${name}`;
+}
+console.log(whois(user));
