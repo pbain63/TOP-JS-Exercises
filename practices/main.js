1bb975836c2e1dcd088a14047f5a8f4140ad2b9a
pbain63 <prodipbain63@gmail.com>
2025-01-25 20:17:37 +0600

    Add JS code for scope practice

--- practices/main.js
+++ practices/main.js
@@ -1,11 +1,9 @@
-const calculator = (function () {
-  const add = (a, b) => a + b;
-  const sub = (a, b) => a - b;
-  const mul = (a, b) => a * b;
-  const div = (a, b) => a / b;
-  return { add, sub, mul, div };
-})();
-console.log(calculator.add(3, 5));
-console.log(calculator.div(14, 5534));
-
-// calculator.sub(6, 2);
+const dog = "snickers";
+function logDog() {
+  console.log(dog);
+}
+function go() {
+  const dog = "sunny";
+  logDog();
+}
+go();
