df844205c3641f295770e69368491dc4ea1274e9
pbain63 <prodipbain63@gmail.com>
2025-01-28 22:08:39 +0600

    Add JS code for module practice

--- practices/main.js
+++ practices/main.js
@@ -1,13 +1,18 @@
 const Formatter = (function () {
+  let timesRun = 0;
   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-
+  const setTimesRun = () => {
+    console.log("Setting times run");
+    ++timesRun;
+  };
   const makeUppercase = (text) => {
     log("Making uppercase");
     return text.toUpperCase();
+    setTimesRun();
+    return text.toUpperCase();
   };
-
-  return {
-    makeUppercase,
-  };
+  return { makeUppercase, timesRun };
 })();
-console.log(Formatter.makeUppercase("tomek"));
+console.log(Formatter.makeUppercase("prodip"));
+Formatter.timesRun = 10;
+console.log(Formatter.timesRun);
