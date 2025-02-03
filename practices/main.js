a923298302c2d709036fb98a9513845945f31f58
pbain63 <prodipbain63@gmail.com>
2025-02-03 09:07:45 +0600

    Add JS code for Factory function practice

--- practices/main.js
+++ practices/main.js
@@ -1,11 +1,24 @@
-const FactoryFunction = (string) => {
-  const capitalizeString = () => string.toUpperCase();
-  const printString = () => console.log(`----${capitalizeString()}----`);
-  return { printString };
-};
-const taco = FactoryFunction("taco");
+// const FactoryFunction = (string) => {
+//   const capitalizeString = () => string.toUpperCase();
+//   const printString = () => console.log(`----${capitalizeString()}----`);
+//   return { printString };
+// };
+// const taco = FactoryFunction("taco");
 
-// printString();
-// capitalizeString();
-// taco.capitalizeString();
-taco.printString();
+// // printString();
+// // capitalizeString();
+// // taco.capitalizeString();
+// taco.printString();
+
+const counterCreator = () => {
+  let count = 0;
+  return () => {
+    console.log(count);
+    count++;
+  };
+};
+const counter = counterCreator();
+counter();
+counter();
+counter();
+counter();
