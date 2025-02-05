accfac1f7dbd08145c5c7e06c5777c0d96c4ea01
pbain63 <prodipbain63@gmail.com>
2025-02-05 21:27:09 +0600

    Add JS code for prototypal inheritance practice

--- practices/main.js
+++ practices/main.js
@@ -1,8 +1,27 @@
-const proto = {
-  hello: function hello() {
-    return `Hello my name is ${this.name}`;
+import Events from "eventemitter3";
+
+const modelMixin = Object.assign(
+  {
+    attrs: {},
+    set(name, value) {
+      this.attrs[name] = value;
+
+      this.emit("change", {
+        prop: name,
+        value: value,
+      });
+    },
+
+    get(name) {
+      return this.attrs[name];
+    },
   },
-};
-const george = Object.assign({}, proto, { name: "George" });
-const msg = george.hello();
-console.log(msg);
+  Events.prototype
+);
+
+const george = { name: "george" };
+const model = Object.assign(george, modelMixin);
+
+model.on("change", (data) => console.log(data));
+
+model.set("name", "Sam");
