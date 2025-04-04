191326d3f1778f9241d9ef20a3536d4832d48292
pbain63 <prodipbain63@gmail.com>
2025-04-04 10:54:28 +0600

    Add JS code for class practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -7,20 +7,14 @@
 //     }
 // };
 
-function User(name, birthday) {
-  this.name = name;
-  this.birthday = birthday;
+class User {
+  constructor(name) {
+    this.name = name;
+  }
 
-  Object.defineProperty(this, "age", {
-    get() {
-      let todayYear = new Date().getFullYear();
-      return todayYear - this.birthday.getFullYear();
-    },
-  });
+  sayHi() {
+    alert(this.name);
+  }
 }
-
-
-
-let john = new User("John", new Date(1992, 6, 1));
-alert(john.birthday);
-alert(john.age);
+let user = new User("Prodip");
+user.sayHi();
