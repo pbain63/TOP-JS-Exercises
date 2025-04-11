746d26a78c3a7758da60eee5efa2e5460c470692
pbain63 <prodipbain63@gmail.com>
2025-04-11 14:10:11 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,29 +1,6 @@
-// let obj = {
-//     get propName() {
-
-//     },
-//     set propName(value) {
-
-//     }
-// };
-
 class User {
-  constructor(name) {
-    this.name = name;
-  }
-  get name() {
-    return this._name;
-  }
-  set name(value) {
-    if (value.length < 4) {
-      alert("Name is too short.");
-      return;
-    }
-    this._name = value;
+  ["say" + "Hi"]() {
+    alert("Prodip");
   }
 }
-
-let user = new User("Prodip");
-alert(user.name);
-
-user = new User("");
+new User().sayHi();
