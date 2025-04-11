0ecfe5a3c6b6a3fd3acd9952653ee4c75dc17b99
pbain63 <prodipbain63@gmail.com>
2025-04-11 08:52:05 +0600

    Add constructor function for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -7,16 +7,11 @@
 //     }
 // };
 
-class User {
-  constructor(name) {
-    this.name = name;
-  }
-  sayHi() {
-    alert(this.name);
-  }
+function User(name) {
+  this.name = name;
 }
-
-alert(typeof User);
-alert(User === User.prototype.constructor);
-alert(User.prototype.sayHi);
-alert(Object.getOwnPropertyNames(User.prototype));
+User.prototype.sayHi = function () {
+  alert(this.name);
+};
+let user = new User("John");
+user.sayHi();
