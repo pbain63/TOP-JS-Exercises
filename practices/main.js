8ba756ec47f8cef112c6c556fbf5d0d05ffeb71b
pbain63 <prodipbain63@gmail.com>
2025-04-12 14:14:39 +0600

    Rewrite a code in class syntax for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,19 +1,38 @@
-class MyClass {
-  prop = value;
-
-  constructor(...){
-
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+    this.timer = null;
   }
 
-  method(...) {
-    
-  }
-  get something (...) {
+  render() {
+    let date = new Date();
 
-  }
-  set something (...) {
+    let hours = date.getHours();
+    if (hours < 10) hours = "0" + hours;
 
+    let mins = date.getMinutes();
+    if (mins < 10) mins = "0" + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = "0" + secs;
+
+    let output = this.template
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", secs);
+
+    console.log(output);
   }
 
-  [Symbol.iterator]() {}
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
 }
+
+let clock = new Clock({ template: "h:m:s" });
+clock.start();
