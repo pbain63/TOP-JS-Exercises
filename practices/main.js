6c68f6e3cfc8cfc4cebf88bff57ed1c6f0fb3ae4
pbain63 <prodipbain63@gmail.com>
2025-04-12 15:19:13 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,38 +1,21 @@
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-    this.timer = null;
-  }
-
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = "0" + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = "0" + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = "0" + secs;
-
-    let output = this.template
-      .replace("h", hours)
-      .replace("m", mins)
-      .replace("s", secs);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
+// Declaration
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
   }
 }
-
-let clock = new Clock({ template: "h:m:s" });
-clock.start();
+// Expression; the class is anonymous but assigned to a variable
+const Rectangle = class {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+// Expression; the class has its own name
+const Rectangle = class Rectangle2 {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
