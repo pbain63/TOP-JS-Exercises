eb4fb2431154e956ea322834a0423533f12ee7a8
pbain63 <prodipbain63@gmail.com>
2025-04-13 12:30:12 +0600

    Add JavaScript code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,21 +1,25 @@
-// Declaration
 class Rectangle {
   constructor(height, width) {
     this.height = height;
     this.width = width;
   }
+
+  get area() {
+    return this.calcArea();
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+  *getSides() {
+    yield this.height;
+    yield this.width;
+    yield this.height;
+    yield this.width;
+  }
 }
-// Expression; the class is anonymous but assigned to a variable
-const Rectangle = class {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
-};
-// Expression; the class has its own name
-const Rectangle = class Rectangle2 {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
-};
+
+const square = new Rectangle(10, 10);
+
+console.log(square.area);
+console.log([...square.getSides()]);
