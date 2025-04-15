5c02b8a8b49c01b553153097df7f876ec83a5462
pbain63 <prodipbain63@gmail.com>
2025-04-15 14:41:21 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,22 +1,18 @@
-class Animal {
+class Cat {
   constructor(name) {
     this.name = name;
   }
-
   speak() {
-    console.log(`${this.name} makes a noise.`);
+    console.log(`${this.name} makes a noise. `);
   }
 }
 
-class Dog extends Animal {
-  constructor(name) {
-    super(name);
-  }
-
+class Lion extends Cat {
   speak() {
-    console.log(`${this.name} barks.`);
+    super.speak();
+    console.log(`${this.name} roars.`);
   }
 }
 
-const d = new Dog("Mitzie");
-d.speak();
+const l = new Lion("Fuzzy");
+l.speak();
