bfb2d9f392d27aba383a15ee681258a118bcd74c
pbain63 <prodipbain63@gmail.com>
2025-04-24 18:54:41 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,16 +1,21 @@
-const Animal = {
-  speak() {
-    console.log(`${this.name} makes a noise.`);
-  },
-};
-
-class Dog {
-  constructor(name) {
-    this.name = name;
+class MyDate extends Date {
+  getFormattedDate() {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return `${this.getDate()}-${months[this.getMonth()]}-${this.getFullYear()}`;
   }
 }
 
-Object.setPrototypeOf(Dog.prototype, Animal);
-
-const d = new Dog("Mitzie");
-d.speak();
+console.log(new MyDate("2025, April, 24 18:52:03").getFormattedDate());
