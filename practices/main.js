47f4f49fc92ddc30cfa775351415aee54e267a73
pbain63 <prodipbain63@gmail.com>
2025-04-27 14:55:37 +0600

    Add JS code for practice in main.js

--- practices/main.js
+++ practices/main.js
@@ -1,9 +1,5 @@
-const numbers = [4, 2, 9, 1, 5];
-let largest = numbers[0];
-
-for (let i = 1; i < numbers.length; i++) {
-  if (numbers[i] > largest) {
-    largest = numbers[i];
-  }
+function reverseString(str) {
+  return str.split("").reverse().join("");
 }
-console.log(largest);//
+
+console.log(reverseString("hello"));
